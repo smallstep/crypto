ff0e9e5feb8fd67cb95dbc3981e0bfe7ad47f718
Mariano Cano <mariano@smallstep.com>
2020-08-13 11:46:08 -0700

    Add pemutil package from github.com/smallstep/cli
    Change cli dependencies with internal packages or callback methods.

--- go.mod
+++ go.mod
@@ -4,7 +4,11 @@ go 1.14
 
 require (
 	github.com/Masterminds/sprig/v3 v3.1.0
+	github.com/kr/pretty v0.1.0 // indirect
 	github.com/pkg/errors v0.9.1
 	github.com/smallstep/assert v0.0.0-20200103212524-b99dc1097b15
 	golang.org/x/crypto v0.0.0-20200414173820-0848c9571904
+	golang.org/x/sys v0.0.0-20200106162015-b016eb3dc98e // indirect
+	gopkg.in/check.v1 v1.0.0-20190902080502-41f04d3bba15 // indirect
+	gopkg.in/yaml.v2 v2.2.7 // indirect
 )
---
+++ internal/utils/io_test.go
@@ -0,0 +1,122 @@
+package utils
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"reflect"
+	"testing"
+
+	"github.com/pkg/errors"
+)
+
+func TestReadFile(t *testing.T) {
+	type args struct {
+		filename string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    []byte
+		wantErr bool
+	}{
+		{"ok", args{"testdata/pass1.txt"}, []byte("brandy-guidon-basin-ishmael-sedge-ducting"), false},
+		{"missing", args{"testdata/missing.txt"}, nil, true},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := ReadFile(tt.args.filename)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("ReadFile() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("ReadFile() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestReadPasswordFromFile(t *testing.T) {
+	type args struct {
+		filename string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    []byte
+		wantErr bool
+	}{
+		{"ok", args{"testdata/pass1.txt"}, []byte("brandy-guidon-basin-ishmael-sedge-ducting"), false},
+		{"trim", args{"testdata/pass2.txt"}, []byte("benumb-eyepiece-stale-revers-marital-mimesis"), false},
+		{"missing", args{"testdata/missing.txt"}, nil, true},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := ReadPasswordFromFile(tt.args.filename)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("ReadPasswordFromFile() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("ReadPasswordFromFile() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestWriteFile(t *testing.T) {
+	tmpDir, err := ioutil.TempDir(os.TempDir(), "go-tests")
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Cleanup(func() {
+		os.RemoveAll(tmpDir)
+	})
+
+	type args struct {
+		filename string
+		data     []byte
+		perm     os.FileMode
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{"ok", args{filepath.Join(tmpDir, "test.txt"), []byte("foo"), 0600}, false},
+		{"fail", args{tmpDir, []byte("foo"), 0600}, true},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := WriteFile(tt.args.filename, tt.args.data, tt.args.perm); (err != nil) != tt.wantErr {
+				t.Errorf("WriteFile() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func Test_maybeUnwrap(t *testing.T) {
+	wantErr := fmt.Errorf("the error")
+
+	type args struct {
+		err error
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr error
+	}{
+		{"wrapped", args{errors.WithMessage(wantErr, "wrapped error")}, wantErr},
+		{"not wrapped", args{wantErr}, wantErr},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			err := maybeUnwrap(tt.args.err)
+			if !reflect.DeepEqual(err, tt.wantErr) {
+				t.Errorf("maybeUnwrap() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
