ae55139291f3d9dec176a1418307cbcc5ad3e8d1
Mariano Cano <mariano@smallstep.com>
2020-08-20 19:26:15 -0700

    Refactor GenerateJWK to use keyutil.

--- jose/generate.go
+++ jose/generate.go
@@ -4,15 +4,13 @@ import (
 	"crypto"
 	"crypto/ecdsa"
 	"crypto/ed25519"
-	"crypto/elliptic"
-	"crypto/rand"
 	"crypto/rsa"
 	"crypto/x509"
 	"encoding/base64"
 
 	"github.com/pkg/errors"
+	"go.step.sm/crypto/keyutil"
 	"go.step.sm/crypto/pemutil"
-	"go.step.sm/crypto/randutil"
 )
 
 const (
@@ -73,18 +71,36 @@ func GenerateDefaultKeyPair(passphrase []byte) (*JSONWebKey, *JSONWebEncryption,
 // GenerateJWK generates a JWK given the key type, curve, alg, use, kid and
 // the size of the RSA or oct keys if necessary.
 func GenerateJWK(kty, crv, alg, use, kid string, size int) (jwk *JSONWebKey, err error) {
-	switch kty {
-	case "EC":
-		return generateECKey(crv, alg, use, kid)
-	case "RSA":
-		return generateRSAKey(size, alg, use, kid)
-	case "OKP":
-		return generateOKPKey(crv, alg, use, kid)
-	case "oct":
-		return generateOctKey(size, alg, use, kid)
-	default:
-		return nil, errors.Errorf("missing or invalid value for flag '--kty'")
+	if kty == "OKP" && use == "enc" && (crv == "" || crv == "Ed25519") {
+		return nil, errors.New("invalid algorithm: Ed25519 cannot be used for encryption")
 	}
+
+	switch {
+	case kty == "EC" && crv == "":
+		crv = P256
+	case kty == "OKP" && crv == "":
+		crv = Ed25519
+	case kty == "RSA" && size == 0:
+		size = DefaultRSASize
+	case kty == "oct" && size == 0:
+		size = DefaultOctSize
+	}
+
+	key, err := keyutil.GenerateKey(kty, crv, size)
+	if err != nil {
+		return nil, err
+	}
+	jwk = &JSONWebKey{
+		Key:       key,
+		KeyID:     kid,
+		Use:       use,
+		Algorithm: alg,
+	}
+	guessJWKAlgorithm(&context{alg: alg}, jwk)
+	if jwk.KeyID == "" && kty != "oct" {
+		jwk.KeyID, err = Thumbprint(jwk)
+	}
+	return jwk, err
 }
 
 // GenerateJWKFromPEM returns an incomplete JSONWebKey using the key from a
@@ -170,128 +186,4 @@ func containsUsage(usage x509.KeyUsage, queries ...x509.KeyUsage) bool {
 		}
 	}
 	return false
-}
-
-func generateECKey(crv, alg, use, kid string) (*JSONWebKey, error) {
-	var c elliptic.Curve
-	var sigAlg string
-	switch crv {
-	case P256, "": // default
-		c, sigAlg = elliptic.P256(), ES256
-	case P384:
-		c, sigAlg = elliptic.P384(), ES384
-	case P521:
-		c, sigAlg = elliptic.P521(), ES512
-	default:
-		return nil, errors.Errorf("missing or invalid value for flag '--crv'")
-	}
-
-	key, err := ecdsa.GenerateKey(c, rand.Reader)
-	if err != nil {
-		return nil, errors.Wrap(err, "error generating ECDSA key")
-	}
-
-	switch use {
-	case "enc":
-		if alg == "" {
-			alg = string(DefaultECKeyAlgorithm)
-		}
-	default:
-		if alg == "" {
-			alg = sigAlg
-		}
-	}
-
-	return &JSONWebKey{
-		Key:       key,
-		Algorithm: alg,
-		Use:       use,
-		KeyID:     kid,
-	}, nil
-}
-
-func generateRSAKey(bits int, alg, use, kid string) (*JSONWebKey, error) {
-	if bits == 0 {
-		bits = DefaultRSASize
-	}
-
-	key, err := rsa.GenerateKey(rand.Reader, bits)
-	if err != nil {
-		return nil, errors.Wrap(err, "error generating RSA key")
-	}
-
-	switch use {
-	case "enc":
-		if alg == "" {
-			alg = string(DefaultRSAKeyAlgorithm)
-		}
-	default:
-		if alg == "" {
-			alg = DefaultRSASigAlgorithm
-		}
-	}
-
-	return &JSONWebKey{
-		Key:       key,
-		Algorithm: alg,
-		Use:       use,
-		KeyID:     kid,
-	}, nil
-}
-
-func generateOKPKey(crv, alg, use, kid string) (*JSONWebKey, error) {
-	switch crv {
-	case Ed25519, "": // default
-		_, key, err := ed25519.GenerateKey(rand.Reader)
-		if err != nil {
-			return nil, errors.Wrap(err, "error generating Ed25519 key")
-		}
-
-		switch use {
-		case "enc":
-			return nil, errors.New("invalid algorithm: Ed25519 cannot be used for encryption")
-		default:
-			if alg == "" {
-				alg = EdDSA
-			}
-		}
-
-		return &JSONWebKey{
-			Key:       key,
-			Algorithm: alg,
-			Use:       use,
-			KeyID:     kid,
-		}, nil
-	default:
-		return nil, errors.Errorf("missing or invalid value for flag '--crv'")
-	}
-}
-
-func generateOctKey(size int, alg, use, kid string) (*JSONWebKey, error) {
-	if size == 0 {
-		size = DefaultOctSize
-	}
-
-	key, err := randutil.Alphanumeric(size)
-	if err != nil {
-		return nil, err
-	}
-
-	switch use {
-	case "enc":
-		if alg == "" {
-			alg = string(DefaultOctKeyAlgorithm)
-		}
-	default:
-		if alg == "" {
-			alg = string(DefaultOctSigsAlgorithm)
-		}
-	}
-
-	return &JSONWebKey{
-		Key:       []byte(key),
-		Algorithm: alg,
-		Use:       use,
-		KeyID:     kid,
-	}, nil
 }
