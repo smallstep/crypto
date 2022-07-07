30ef45451b3e4253d2f9758cd6093acf7f3be3ac
Mariano Cano <mariano.cano@gmail.com>
2022-07-07 15:01:31 -0700

    Add kmsfs unit tests

---
+++ kms/kmfs_test.go
@@ -0,0 +1,316 @@
+package kms
+
+import (
+	"context"
+	"crypto"
+	"crypto/x509"
+	"crypto/x509/pkix"
+	"errors"
+	"io/fs"
+	"os"
+	"reflect"
+	"testing"
+
+	"go.step.sm/crypto/kms/apiv1"
+	"go.step.sm/crypto/kms/softkms"
+)
+
+type fakeCM struct {
+	softkms.SoftKMS
+}
+
+func (f *fakeCM) GetPublicKey(req *apiv1.GetPublicKeyRequest) (crypto.PublicKey, error) {
+	if req.Name == "fail" {
+		return nil, errors.New("an error")
+	}
+	return []byte(req.Name), nil
+}
+
+func (f *fakeCM) LoadCertificate(req *apiv1.LoadCertificateRequest) (*x509.Certificate, error) {
+	if req.Name == "fail" {
+		return nil, errors.New("an error")
+	}
+
+	return &x509.Certificate{Subject: pkix.Name{CommonName: req.Name}}, nil
+}
+
+func (f *fakeCM) StoreCertificate(req *apiv1.StoreCertificateRequest) error {
+	if req.Name == "fail" {
+		return errors.New("an error")
+	}
+
+	return nil
+}
+
+func TestMain(m *testing.M) {
+	apiv1.Register(apiv1.Type("fake"), func(ctx context.Context, opts apiv1.Options) (apiv1.KeyManager, error) {
+		return &fakeCM{}, nil
+	})
+	os.Exit(m.Run())
+}
+
+func Test_new(t *testing.T) {
+	ctx := context.TODO()
+	type args struct {
+		ctx    context.Context
+		kmsuri string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    *kmsfs
+		wantErr bool
+	}{
+		{"ok empty", args{ctx, ""}, &kmsfs{}, false},
+		{"ok softkms", args{ctx, "softkms:"}, &kmsfs{
+			KeyManager: &softkms.SoftKMS{},
+		}, false},
+		{"fail", args{ctx, "fail:"}, nil, true},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := new(tt.args.ctx, tt.args.kmsuri)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("new() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("new() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_kmsfs_getKMS(t *testing.T) {
+	type fields struct {
+		KeyManager apiv1.KeyManager
+	}
+	type args struct {
+		kmsuri string
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		want    apiv1.KeyManager
+		wantErr bool
+	}{
+		{"ok empty", fields{nil}, args{""}, &softkms.SoftKMS{}, false},
+		{"ok softkms", fields{&softkms.SoftKMS{}}, args{""}, &softkms.SoftKMS{}, false},
+		{"ok softkms with uri", fields{nil}, args{"softkms:"}, &softkms.SoftKMS{}, false},
+		{"fail", fields{nil}, args{"fail:"}, nil, true},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			f := &kmsfs{
+				KeyManager: tt.fields.KeyManager,
+			}
+			got, err := f.getKMS(tt.args.kmsuri)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("kmsfs.getKMS() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("kmsfs.getKMS() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_loadKMS(t *testing.T) {
+	ctx := context.TODO()
+	type args struct {
+		ctx    context.Context
+		kmsuri string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    apiv1.KeyManager
+		wantErr bool
+	}{
+		{"ok", args{ctx, "softkms:"}, &softkms.SoftKMS{}, false},
+		{"ok empty", args{ctx, ""}, &softkms.SoftKMS{}, false},
+		{"fail", args{ctx, "fail:"}, nil, true},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := loadKMS(tt.args.ctx, tt.args.kmsuri)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("loadKMS() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("loadKMS() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_openError(t *testing.T) {
+	type args struct {
+		name string
+		err  error
+	}
+	tests := []struct {
+		name string
+		args args
+		want *fs.PathError
+	}{
+		{"ok", args{"name", errors.New("an error")}, &fs.PathError{
+			Path: "name", Op: "open", Err: errors.New("an error"),
+		}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := openError(tt.args.name, tt.args.err); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("openError() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestCertFS(t *testing.T) {
+	ctx := context.TODO()
+	type args struct {
+		ctx    context.Context
+		kmsuri string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    fs.FS
+		wantErr bool
+	}{
+		{"ok", args{ctx, "fake:"}, &certFS{kmsfs: &kmsfs{KeyManager: &fakeCM{}}}, false},
+		{"fail", args{ctx, "fail:"}, nil, true},
+		{"fail not implemented", args{ctx, "softkms:"}, nil, true},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := CertFS(tt.args.ctx, tt.args.kmsuri)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("CertFS() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("CertFS() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_certFS_Open(t *testing.T) {
+	fake := &kmsfs{KeyManager: &fakeCM{}}
+	type fields struct {
+		kmsfs *kmsfs
+	}
+	type args struct {
+		name string
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		want    fs.File
+		wantErr bool
+	}{
+		{"ok", fields{fake}, args{"foo"}, &object{
+			Path:   "foo",
+			Object: &x509.Certificate{Subject: pkix.Name{CommonName: "foo"}},
+		}, false},
+		{"ok load", fields{&kmsfs{}}, args{"fake:foo"}, &object{
+			Path:   "fake:foo",
+			Object: &x509.Certificate{Subject: pkix.Name{CommonName: "fake:foo"}},
+		}, false},
+		{"fail fake", fields{fake}, args{"fail"}, nil, true},
+		{"fail unregistered", fields{&kmsfs{}}, args{"fail:"}, nil, true},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			f := &certFS{
+				kmsfs: tt.fields.kmsfs,
+			}
+			got, err := f.Open(tt.args.name)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("certFS.Open() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("certFS.Open() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestKeyFS(t *testing.T) {
+	ctx := context.TODO()
+	type args struct {
+		ctx    context.Context
+		kmsuri string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    fs.FS
+		wantErr bool
+	}{
+		{"ok", args{ctx, "fake:"}, &keyFS{kmsfs: &kmsfs{KeyManager: &fakeCM{}}}, false},
+		{"fail", args{ctx, "fail:"}, nil, true},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := KeyFS(tt.args.ctx, tt.args.kmsuri)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("KeyFS() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("KeyFS() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_keyFS_Open(t *testing.T) {
+	fake := &kmsfs{KeyManager: &fakeCM{}}
+	type fields struct {
+		kmsfs *kmsfs
+	}
+	type args struct {
+		name string
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		want    fs.File
+		wantErr bool
+	}{
+		{"ok", fields{fake}, args{"foo"}, &object{
+			Path:   "foo",
+			Object: []byte("foo"),
+		}, false},
+		{"ok load", fields{&kmsfs{}}, args{"fake:foo"}, &object{
+			Path:   "fake:foo",
+			Object: []byte("fake:foo"),
+		}, false},
+		{"fail fake", fields{fake}, args{"fail"}, nil, true},
+		{"fail unregistered", fields{&kmsfs{}}, args{"fail:"}, nil, true},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			f := &keyFS{
+				kmsfs: tt.fields.kmsfs,
+			}
+			got, err := f.Open(tt.args.name)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("keyFS.Open() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("keyFS.Open() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
---
+++ kms/object_test.go
@@ -0,0 +1,243 @@
+package kms
+
+import (
+	"bytes"
+	"crypto"
+	"crypto/ed25519"
+	"crypto/rand"
+	"encoding/pem"
+	"io"
+	"io/fs"
+	"reflect"
+	"sync"
+	"testing"
+	"time"
+
+	"go.step.sm/crypto/pemutil"
+)
+
+func generateKey(t *testing.T) (crypto.PublicKey, *bytes.Buffer) {
+	t.Helper()
+
+	pub, _, err := ed25519.GenerateKey(rand.Reader)
+	if err != nil {
+		t.Fatal(err)
+	}
+	block, err := pemutil.Serialize(pub)
+	if err != nil {
+		t.Fatal(err)
+	}
+	return pub, bytes.NewBuffer(pem.EncodeToMemory(block))
+}
+
+func Test_object_FileMode(t *testing.T) {
+	pub, pemData := generateKey(t)
+	type fields struct {
+		Path    string
+		Object  interface{}
+		pemData *bytes.Buffer
+	}
+	tests := []struct {
+		name        string
+		fields      fields
+		wantName    string
+		wantSize    int64
+		wantMode    fs.FileMode
+		wantModTime time.Time
+		wantIsDir   bool
+		wantSys     interface{}
+	}{
+		{"ok", fields{"path", pub, pemData}, "path", int64(pemData.Len()), 0400, time.Time{}, false, pub},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			o := &object{
+				Path:    tt.fields.Path,
+				Object:  tt.fields.Object,
+				pemData: tt.fields.pemData,
+			}
+			if got := o.Name(); got != tt.wantName {
+				t.Errorf("object.Name() = %v, want %v", got, tt.wantName)
+			}
+			if got := o.Size(); got != tt.wantSize {
+				t.Errorf("object.Size() = %v, want %v", got, tt.wantSize)
+			}
+			if got := o.Mode(); got != tt.wantMode {
+				t.Errorf("object.Mode() = %v, want %v", got, tt.wantMode)
+			}
+			if got := o.ModTime(); got != tt.wantModTime {
+				t.Errorf("object.ModTime() = %v, want %v", got, tt.wantModTime)
+			}
+			if got := o.IsDir(); got != tt.wantIsDir {
+				t.Errorf("object.IsDir() = %v, want %v", got, tt.wantIsDir)
+			}
+			if got := o.Sys(); !reflect.DeepEqual(got, tt.wantSys) {
+				t.Errorf("object.Sys() = %v, want %v", got, tt.wantSys)
+			}
+		})
+	}
+}
+
+func Test_object_load(t *testing.T) {
+	pub, pemData := generateKey(t)
+	type fields struct {
+		Path   string
+		Object interface{}
+	}
+	tests := []struct {
+		name        string
+		fields      fields
+		wantPemData *bytes.Buffer
+		wantErr     bool
+	}{
+		{"ok", fields{"path", pub}, pemData, false},
+		{"fail", fields{"path", "not a key"}, nil, true},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			o := &object{
+				Path:   tt.fields.Path,
+				Object: tt.fields.Object,
+			}
+			if err := o.load(); (err != nil) != tt.wantErr {
+				t.Errorf("object.load() error = %v, wantErr %v", err, tt.wantErr)
+			}
+			if !reflect.DeepEqual(o.pemData, tt.wantPemData) {
+				t.Errorf("object.load() pemData = %s, wantPemData %s", o.pemData, tt.wantPemData)
+			}
+		})
+	}
+}
+
+func Test_object_Stat(t *testing.T) {
+	pub, pemData := generateKey(t)
+	type fields struct {
+		Path   string
+		Object interface{}
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		want    fs.FileInfo
+		wantErr bool
+	}{
+		{"ok", fields{"path", pub}, &object{
+			Path:    "path",
+			Object:  pub,
+			pemData: pemData,
+		}, false},
+		{"fail", fields{"path", "not a key"}, nil, true},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			o := &object{
+				Path:   tt.fields.Path,
+				Object: tt.fields.Object,
+			}
+			got, err := o.Stat()
+			if (err != nil) != tt.wantErr {
+				t.Errorf("object.Stat() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			// Normalize
+			if got != nil {
+				got.(*object).once = sync.Once{}
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("object.Stat() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_object_Read(t *testing.T) {
+	pub, pemData := generateKey(t)
+	type fields struct {
+		Path   string
+		Object interface{}
+	}
+	type args struct {
+		b []byte
+	}
+	tests := []struct {
+		name      string
+		fields    fields
+		args      args
+		wantN     int
+		wantBytes []byte
+		wantErr   bool
+	}{
+		{"ok", fields{"path", pub}, args{make([]byte, pemData.Len())}, pemData.Len(), pemData.Bytes(), false},
+		{"empty", fields{"path", pub}, args{[]byte{}}, 0, []byte{}, false},
+		{"fail", fields{"path", "not a key"}, args{[]byte{}}, 0, []byte{}, true},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			o := &object{
+				Path:   tt.fields.Path,
+				Object: tt.fields.Object,
+			}
+			got, err := o.Read(tt.args.b)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("object.Read() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.wantN {
+				t.Errorf("object.Read() = %v, wanN %v", got, tt.wantN)
+			}
+			if !bytes.Equal(tt.args.b, tt.wantBytes) {
+				t.Errorf("object.Read() = %v, wantBytes %v", tt.args.b, tt.wantBytes)
+			}
+		})
+	}
+}
+
+func Test_object_Close(t *testing.T) {
+	pub, _ := generateKey(t)
+	o := &object{
+		Path:   "path",
+		Object: pub,
+	}
+	if err := o.load(); err != nil {
+		t.Fatal(err)
+	}
+
+	type fields struct {
+		Path    string
+		Object  interface{}
+		once    sync.Once
+		err     error
+		pemData *bytes.Buffer
+	}
+	tests := []struct {
+		name    string
+		o       *object
+		want    *object
+		wantErr bool
+	}{
+		{"ok", o, &object{
+			Path:    "path",
+			Object:  nil,
+			pemData: nil,
+			err:     io.EOF,
+		}, false},
+		{"eof", o, &object{
+			Path:    "path",
+			Object:  nil,
+			pemData: nil,
+			err:     io.EOF,
+		}, true},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := tt.o.Close(); (err != nil) != tt.wantErr {
+				t.Errorf("object.Close() error = %v, wantErr %v", err, tt.wantErr)
+			}
+			// Normalize
+			tt.o.once = sync.Once{}
+			if !reflect.DeepEqual(tt.o, tt.want) {
+				t.Errorf("object.Close() = %v, want %v", tt.o, tt.want)
+			}
+		})
+	}
+}
