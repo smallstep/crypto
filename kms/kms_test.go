cef82d7049275e297e4c638b6456ef2a684edfc4
Mariano Cano <mariano.cano@gmail.com>
2022-11-02 12:33:12 -0700

    Remove test case that fails with coverpkg
    It removes a test case that checks that a kms is not available if
    not imported. Running go test with -coverpkg=./... will automatically
    import pkcs11, and the test will fail.

--- kms/kms_test.go
+++ kms/kms_test.go
@@ -40,7 +40,6 @@ func TestNew(t *testing.T) {
 		{"uri", false, args{ctx, apiv1.Options{URI: "softkms:foo=bar"}}, &softkms.SoftKMS{}, false},
 		{"awskms", false, args{ctx, apiv1.Options{Type: "awskms"}}, &awskms.KMS{}, false},
 		{"cloudkms", true, args{ctx, apiv1.Options{Type: "cloudkms"}}, &cloudkms.CloudKMS{}, failCloudKMS},
-		{"fail not enabled", false, args{ctx, apiv1.Options{Type: "pkcs11"}}, nil, true}, // not enabled
 		{"fail validation", false, args{ctx, apiv1.Options{Type: "foobar"}}, nil, true},
 	}
 	for _, tt := range tests {
