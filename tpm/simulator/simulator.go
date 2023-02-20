9271ec66f0aa34d3e1968cb5cd89102e7231a642
Herman Slatman <hermanslatman@hotmail.com>
2023-02-20 21:08:35 +0100

    Include `github.com/google/go-tpm-tools/simulator` as dependency
    The `simulator` from `go-tpm-tools` is included as a copy to make
    it easier to work with the TPM simulator written in C.

---
+++ tpm/simulator/simulator.go
@@ -0,0 +1,63 @@
+package simulator
+
+import (
+	"context"
+	"fmt"
+	"io"
+
+	gotpm "go.step.sm/crypto/tpm/simulator/internal"
+)
+
+type Simulator struct {
+	wrapped *gotpm.Simulator
+}
+
+func New() *Simulator {
+	return &Simulator{}
+}
+
+func (s *Simulator) Open(ctx context.Context) error {
+	var sim *gotpm.Simulator
+	var err error
+	if s.wrapped == nil {
+		sim, err = gotpm.Get()
+		if err != nil {
+			return err
+		}
+	}
+
+	s.wrapped = sim
+	return nil
+}
+
+func (s *Simulator) Close() error {
+	if s.wrapped == nil {
+		return nil
+	}
+
+	if s.wrapped.IsClosed() {
+		return nil
+	}
+
+	if err := s.wrapped.Close(); err != nil {
+		return fmt.Errorf("failed closing TPM simulator: %w", err)
+	}
+
+	s.wrapped = nil
+
+	return nil
+}
+
+func (s *Simulator) MeasurementLog() ([]byte, error) {
+	return nil, nil
+}
+
+func (s *Simulator) Read(p []byte) (n int, err error) {
+	return s.wrapped.Read(p)
+}
+
+func (s *Simulator) Write(p []byte) (n int, err error) {
+	return s.wrapped.Write(p)
+}
+
+var _ io.ReadWriteCloser = (*Simulator)(nil)
