769c7e9bd4c4d9b55b5fe4e0f73c0cb1aedee5d1
Herman Slatman <hermanslatman@hotmail.com>
2023-05-17 15:43:18 +0200

    Validate RSA decryption options for Google Cloud KMS

--- kms/cloudkms/decrypter.go
+++ kms/cloudkms/decrypter.go
@@ -2,19 +2,21 @@ package cloudkms
 
 import (
 	"crypto"
+	"crypto/rsa"
 	"errors"
 	"fmt"
 	"hash/crc32"
 	"io"
 
 	"cloud.google.com/go/kms/apiv1/kmspb"
+	"google.golang.org/protobuf/types/known/wrapperspb"
+
 	"go.step.sm/crypto/kms/apiv1"
 	"go.step.sm/crypto/pemutil"
-	"google.golang.org/protobuf/types/known/wrapperspb"
 )
 
 // CreateDecrypter implements the apiv1.Decrypter interface and returns
-// a crypto.Decrypter backed by Google Cloud KMS.
+// a crypto.Decrypter backed by a decryption key in Google Cloud KMS.
 func (k *CloudKMS) CreateDecrypter(req *apiv1.CreateDecrypterRequest) (crypto.Decrypter, error) {
 	if req.DecryptionKey == "" {
 		return nil, errors.New("decryption key cannot be empty")
@@ -29,11 +31,12 @@ type Decrypter struct {
 	publicKey     crypto.PublicKey
 }
 
-// NewDecrypter creates a new crypto.Decrypter the given CloudKMS decryption key.
-func NewDecrypter(c KeyManagementClient, decryptionKey string) (*Decrypter, error) {
+// NewDecrypter creates a new crypto.Decrypter backed by the given
+// Google Cloud KMS decryption key.
+func NewDecrypter(client KeyManagementClient, decryptionKey string) (*Decrypter, error) {
 	// Make sure that the key exists.
 	decrypter := &Decrypter{
-		client:        c,
+		client:        client,
 		decryptionKey: decryptionKey,
 	}
 	if err := decrypter.preloadKey(decryptionKey); err != nil { // TODO(hs): (option for) lazy load instead?
@@ -63,13 +66,31 @@ func (d *Decrypter) Public() crypto.PublicKey {
 	return d.publicKey
 }
 
-func (d *Decrypter) Decrypt(rand io.Reader, msg []byte, opts crypto.DecrypterOpts) (plaintext []byte, err error) {
-	// TODO(hs): detect if the ciphertext matches the key type and abort if
-	// some unsupported combination and/or operation is used?
-	ciphertextCRC32C := crc32c(msg)
+// Decrypt decrypts ciphertext using the decryption key backed by Google Cloud KMS and returns
+// the plaintext bytes. An error is returned when decryption fails. Google Cloud KMS only supports
+// RSA keys with 2048, 3072 or 4096 bits and will always use OAEP. Labels are not supported.
+//
+// Also see https://cloud.google.com/kms/docs/algorithms#asymmetric_encryption_algorithms.
+func (d *Decrypter) Decrypt(rand io.Reader, ciphertext []byte, opts crypto.DecrypterOpts) ([]byte, error) {
+	if ropts, ok := opts.(*rsa.OAEPOptions); ok {
+		if len(ropts.Label) > 0 {
+			return nil, errors.New("cloudKMS does not support RSA-OAEP label")
+		}
+		switch ropts.Hash {
+		case crypto.SHA1, crypto.SHA256, crypto.SHA512:
+			break
+		default:
+			return nil, fmt.Errorf("cloudKMS does not support hash algorithm %q with RSA-OAEP", ropts.Hash)
+		}
+	}
+	if _, ok := opts.(*rsa.PKCS1v15DecryptOptions); ok {
+		return nil, errors.New("cloudKMS does not support PKCS #1 v1.5 decryption")
+	}
+
+	ciphertextCRC32C := crc32c(ciphertext)
 	req := &kmspb.AsymmetricDecryptRequest{
 		Name:             d.decryptionKey,
-		Ciphertext:       msg,
+		Ciphertext:       ciphertext,
 		CiphertextCrc32C: wrapperspb.Int64(int64(ciphertextCRC32C)),
 	}
 
