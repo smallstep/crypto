a9d2714473aba335dba42bb3b00a9aaf778124c2
Herman Slatman <hermanslatman@hotmail.com>
2023-07-24 17:04:25 +0200

    Add new option tests for `New` for the `TPMKMS`

--- kms/tpmkms/tpmkms_test.go
+++ kms/tpmkms/tpmkms_test.go
@@ -18,9 +18,10 @@ func TestNew(t *testing.T) {
 		want    *TPMKMS
 		wantErr bool
 	}{
-		{"ok/defaults", args{apiv1.Options{Type: "tpmkms"}}, &TPMKMS{}, false},
-		{"ok/uri", args{apiv1.Options{Type: "tpmkms", URI: "tpmkms:device=/dev/tpm0;storage-directory=/tmp/tpmstorage"}}, &TPMKMS{}, false},
+		{"ok/defaults", args{apiv1.Options{Type: "tpmkms"}}, &TPMKMS{identityEarlyRenewalEnabled: true, identityRenewalPeriodPercentage: 60}, false},
+		{"ok/uri", args{apiv1.Options{Type: "tpmkms", URI: "tpmkms:device=/dev/tpm0;storage-directory=/tmp/tpmstorage;renewal-percentage=70"}}, &TPMKMS{identityEarlyRenewalEnabled: true, identityRenewalPeriodPercentage: 70}, false},
 		{"fail/uri-scheme", args{apiv1.Options{Type: "tpmkms", URI: "tpmkmz://device=/dev/tpm0"}}, &TPMKMS{}, true},
+		{"fail/renewal-percentage-too-low", args{apiv1.Options{Type: "tpmkms", URI: "tpmkms:renewal-percentage=0"}}, &TPMKMS{}, true},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -32,6 +33,8 @@ func TestNew(t *testing.T) {
 
 			if assert.NotNil(t, got) {
 				assert.NotNil(t, got.tpm)
+				assert.Equal(t, tt.want.identityEarlyRenewalEnabled, got.identityEarlyRenewalEnabled)
+				assert.Equal(t, tt.want.identityRenewalPeriodPercentage, got.identityRenewalPeriodPercentage)
 			}
 		})
 	}
