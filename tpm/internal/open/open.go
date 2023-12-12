f3ef00c61ac6e852fec15ae3522d5a5abca8028c
Herman Slatman <hermanslatman@hotmail.com>
2023-12-12 22:41:33 +0100

    Add support for `mssim:` for non attestation TPM too

--- tpm/internal/open/open.go
+++ tpm/internal/open/open.go
@@ -1,9 +1,28 @@
 package open
 
 import (
+	"fmt"
 	"io"
+	"strings"
+
+	"go.step.sm/crypto/kms/uri"
+	"go.step.sm/crypto/tpm/internal/mssim"
 )
 
 func TPM(deviceName string) (io.ReadWriteCloser, error) {
+	if deviceName != "" {
+		if strings.HasPrefix(deviceName, "mssim:") {
+			u, err := uri.ParseWithScheme("mssim", deviceName)
+			if err != nil {
+				return nil, fmt.Errorf("failed parsing %q: %w", deviceName, err)
+			}
+			rwc, err := mssim.New(u)
+			if err != nil {
+				return nil, err
+			}
+			return rwc, nil
+		}
+	}
+
 	return open(deviceName)
 }
