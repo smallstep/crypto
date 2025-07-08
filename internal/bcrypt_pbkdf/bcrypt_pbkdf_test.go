1e7ef83d8e00fe661d3edc4f1faf42e3dc1bac00
Herman Slatman <hermanslatman@hotmail.com>
2025-07-08 10:52:57 +0200

    Remove underscore from `bcrypt_pbkdf` package

--- internal/bcrypt_pbkdf/bcrypt_pbkdf_test.go
+++ internal/bcrypt_pbkdf/bcrypt_pbkdf_test.go
@@ -1,9 +1,7 @@
 // Copyright 2014 Dmitry Chestnykh. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
-//
-//nolint:staticcheck // ignore underscore in package
-package bcrypt_pbkdf
+package bcryptpbkdf
 
 import (
 	"bytes"
