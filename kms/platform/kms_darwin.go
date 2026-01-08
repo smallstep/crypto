d0d6ca3ac9ec9175e7d7b9559af4936fe73b77e2
Mariano Cano <mariano.cano@gmail.com>
2026-01-08 13:05:04 -0800

    wip

--- kms/platform/kms_darwin.go
+++ kms/platform/kms_darwin.go
@@ -2,32 +2,62 @@ package platform
 
 import (
 	"context"
+	"fmt"
+	"net/url"
 
 	"go.step.sm/crypto/kms/apiv1"
 	"go.step.sm/crypto/kms/mackms"
+	"go.step.sm/crypto/kms/uri"
 )
 
 var _ apiv1.SearchableKeyManager = (*KMS)(nil)
 
 func newKMS(ctx context.Context, opts apiv1.Options) (*KMS, error) {
-	if opts.Type == apiv1.TPMKMS {
-		return newTPMKMS(ctx, opts)
+	if opts.URI == "" {
+		return newMacKMS(ctx, opts)
 	}
 
+	u, err := parseURI(opts.URI)
+	if err != nil {
+		return nil, err
+	}
+
+	switch u.backend {
+	case apiv1.TPMKMS:
+		return newTPMKMS(ctx, opts)
+	case apiv1.DefaultKMS, apiv1.MacKMS:
+		opts.URI = transformToMacKMS(u)
+		return newMacKMS(ctx, opts)
+	default:
+		return nil, fmt.Errorf("failed parsing %q: unsupported backend %q", opts.URI, u.backend)
+	}
+}
+
+func newMacKMS(ctx context.Context, opts apiv1.Options) (*KMS, error) {
 	km, err := mackms.New(ctx, opts)
 	if err != nil {
 		return nil, err
 	}
 
 	return &KMS{
-		backend: km,
+		backend:      km,
+		transformURI: transformToMacKMS,
 	}, nil
 }
 
-func (k *KMS) SearchKeys(req *apiv1.SearchKeysRequest) (*apiv1.SearchKeysResponse, error) {
-	if km, ok := k.backend.(apiv1.SearchableKeyManager); ok {
-		return km.SearchKeys(req)
+func transformToMacKMS(u *kmsURI) string {
+	uv := url.Values{
+		"label": []string{u.name},
+	}
+	if u.hw {
+		uv.Set("se", "true")
+		uv.Set("keychain", "dataProtection")
 	}
 
-	return nil, apiv1.NotImplementedError{}
+	// Add custom extra values that might be mackms specific.
+	for k, v := range u.extraValues {
+		uv[k] = v
+	}
+
+	return uri.New(mackms.Scheme, uv).String()
 }
--- kms/platform/kms_windows.go
+++ kms/platform/kms_windows.go
@@ -4,6 +4,8 @@ package platform
 
 import (
 	"context"
+	"fmt"
+	"net/url"
 
 	"go.step.sm/crypto/kms/apiv1"
 	"go.step.sm/crypto/kms/capi"
@@ -11,28 +13,47 @@ import (
 )
 
 func newKMS(ctx context.Context, opts apiv1.Options) (*KMS, error) {
-	if opts.Type == apiv1.CAPIKMS {
-		km, err := capi.New(ctx, opts)
-		if err != nil {
-			return nil, err
-		}
-
-		return &KMS{
-			backend: km,
-		}, nil
+	if opts.URI == "" {
+		return newTPMKMS(ctx, opts)
 	}
 
-	if opts.URI != "" {
-		u, err := uri.Parse(opts.URI)
-		if err != nil {
-			return nil, err
-		}
-
-		if !u.Has("enable-cng") {
-			u.Values.Set("enable-cng", "true")
-		}
-		opts.URI = u.String()
+	u, err := parseURI(opts.URI)
+	if err != nil {
+		return nil, err
 	}
 
-	return newTPMKMS(ctx, opts)
+	switch u.backend {
+	case apiv1.CAPIKMS:
+		opts.URI = transformToCapiKMS(u)
+		return newCAPIKMS(ctx, opts)
+	case apiv1.DefaultKMS, apiv1.TPMKMS:
+		return newTPMKMS(ctx, opts)
+	default:
+		return nil, fmt.Errorf("failed parsing %q: unsupported backend %q", opts.URI, u.backend)
+	}
+}
+
+func newCAPIKMS(ctx context.Context, opts apiv1.Options) (*KMS, error) {
+	km, err := capi.New(ctx, opts)
+	if err != nil {
+		return nil, err
+	}
+
+	return &KMS{
+		backend:      km,
+		transformURI: transformToCapiKMS,
+	}, nil
+}
+
+func transformToCapiKMS(u *kmsURI) string {
+	uv := url.Values{
+		"key": []string{u.name},
+	}
+
+	// Add custom extra values that might be tpmkms specific.
+	for k, v := range u.extraValues {
+		uv[k] = v
+	}
+
+	return uri.New(capi.Scheme, uv).String()
 }
