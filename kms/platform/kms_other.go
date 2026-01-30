81f376bd614bbf384467d13db56fa66c1cfe6d84
Mariano Cano <mariano.cano@gmail.com>
2026-01-29 17:39:05 -0800

    Add softkms as a backend for platformkms

--- kms/platform/kms_other.go
+++ kms/platform/kms_other.go
@@ -4,10 +4,27 @@ package platform
 
 import (
 	"context"
+	"fmt"
 
 	"go.step.sm/crypto/kms/apiv1"
 )
 
 func newKMS(ctx context.Context, opts apiv1.Options) (*KMS, error) {
-	return newTPMKMS(ctx, opts)
+	if opts.URI == "" {
+		return newTPMKMS(ctx, opts)
+	}
+
+	u, err := parseURI(opts.URI)
+	if err != nil {
+		return nil, err
+	}
+
+	switch u.backend {
+	case apiv1.SoftKMS:
+		return newSoftKMS(ctx, opts)
+	case apiv1.DefaultKMS, apiv1.TPMKMS:
+		return newTPMKMS(ctx, opts)
+	default:
+		return nil, fmt.Errorf("failed parsing %q: unsupported backend %q", opts.URI, u.backend)
+	}
 }
